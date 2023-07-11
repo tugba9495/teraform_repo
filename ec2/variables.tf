4d18814e9efd52c6e6453ff31f7bf3fef8543f7a
tugba9495 <tuba_7655@icloud.com>
2023-07-11 18:09:23 -0500

    modified some files

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,37 +1,37 @@
 variable "instance_type" {
-    type = string
-    description = "defines instance type"
-    default = "t2.micro"
+    type                 = string
+    description          = "defines instance type"
+    default              = "t2.micro"
   
 }
 variable "key_name" {
-    type = string
-    description = "defines key name"
-    default = "tugba_mac_key"
+    type                 = string
+    description          = "defines key name"
+    default              = "tugba_mac_key"
   
 }
 variable "from_port" {
-    type = number
-    description = "from port foe security group"
-    default = 22
+    type                 = number
+    description          = "from port foe security group"
+    default              = 22
   
 }
 variable "to_port" {
-    type = number
-    description = "to port foe security group"
-    default = 22
+    type                 = number
+    description          = "to port foe security group"
+    default              = 22
   
 }
 variable "protocol" {
-    type = string
-    description = "protocol for security group"
-    default = "tcp"
+    type                 = string
+    description          = "protocol for security group"
+    default              = "tcp"
 
 }
 variable "cidr_blocks" {
-    type = list(string)
-    description = "cidr blocks for security group"
-  default = ["0.0.0.0/0"]
+    type                 = list(string)
+    description          = "cidr blocks for security group"
+  default                = ["0.0.0.0/0"]
 }
 
 
