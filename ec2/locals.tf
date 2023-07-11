1832fed4e9000344fc728478a4f58bf6c37338d4
tugba9495 <tuba_7655@icloud.com>
2023-07-11 18:06:41 -0500

    modified some files

--- ec2/locals.tf
+++ ec2/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  common_tags = {
-    Owner     = "tugba"
-    ManagedBy = "https://github.com/tugba9495/teraform_repo"
+  common_tags       = {
+    Owner           = "tugba"
+    ManagedBy       = "https://github.com/tugba9495/teraform_repo"
   }
 }
 
