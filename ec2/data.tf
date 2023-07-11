b7ac23bcdd980496fa8dfa4556ba7f24d1e0b223
tugba9495 <tuba_7655@icloud.com>
2023-07-11 18:06:19 -0500

    modified some files

--- ec2/data.tf
+++ ec2/data.tf
@@ -1,20 +1,20 @@
 data "aws_ami" "terraform_ami" {
-  most_recent = true
+  most_recent       = true
 
   filter {
-    name   = "architecture"
-    values = ["x86_64"]
+    name            = "architecture"
+    values          = ["x86_64"]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name           = "virtualization-type"
+    values         = ["hvm"]
   }
 
-  owners = ["amazon"]
+  owners           = ["amazon"]
 }
 data "aws_vpc" "terraform_vpc_id" {
-    default = true
+    default        = true
  
 
     
