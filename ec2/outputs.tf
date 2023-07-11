2b07211ebde87983bd469de40c72bc9b4a966549
tugba9495 <tuba_7655@icloud.com>
2023-07-11 18:07:11 -0500

    modified some files

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,8 +1,8 @@
 output "aws_ami" {
-    value = aws_instance.terraform_instance.id
+    value     = aws_instance.terraform_instance.id
   
 }
 output "aws_security_group" {
-    value = aws_security_group.terraform_security_group.id
+    value     = aws_security_group.terraform_security_group.id
   
 }
